5dd0f04d3af0a327c344b9fd3f253c91c0dc91b1
JM <woalsdl600@gmail.com>
2018-11-17 10:29:05 +0900

    UPDATE README aduino 소스 추가

--- aduino_ex/aduino_ex.ino
+++ aduino_ex/aduino_ex.ino
@@ -42,27 +42,49 @@
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
-  Serial.begin(9600);   // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init();   // Init MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+   Serial.begin(9600);      // Initialize serial communications with the PC
+   while (!Serial);      // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+   SPI.begin();         // Init SPI bus
+   mfrc522.PCD_Init();      // Init MFRC522
+   mfrc522.PCD_DumpVersionToSerial();   // Show details of PCD - MFRC522 Card Reader details
+   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
+   // Look for new cards
+   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+      return;
+   }
 
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+   // Select one of the cards
+   if ( ! mfrc522.PICC_ReadCardSerial()) {
+      return;
+   }
 
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+   // Dump debug info about the card; PICC_HaltA() is automatically called
+   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
+  /*Serial.print("UID tag : ");
+  String content = "";
   
+  for(byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+
+  Serial.print("Message : ");
+  content.toUpperCase();
+
+  if(content.substring(1) == "") {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(1000);
+  }
+  else {
+    Serial.println("Access denied");
+    delay(1000);
+  }*/
 }
