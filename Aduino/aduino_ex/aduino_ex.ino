330c8286d874bc4a04f9bfaf0c065299ccee5db1
JM <woalsdl600@gmail.com>
2018-11-17 11:31:31 +0900

    UPDATE

--- Aduino/aduino_ex/aduino_ex.ino
+++ Aduino/aduino_ex/aduino_ex.ino
@@ -1,25 +1,4 @@
 /*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- * 
- * @license Released into the public domain.
- * 
  * Typical pin layout used:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
@@ -47,7 +26,6 @@ void setup() {
    SPI.begin();         // Init SPI bus
    mfrc522.PCD_Init();      // Init MFRC522
    mfrc522.PCD_DumpVersionToSerial();   // Show details of PCD - MFRC522 Card Reader details
-   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
@@ -62,9 +40,9 @@ void loop() {
    }
 
    // Dump debug info about the card; PICC_HaltA() is automatically called
-   mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+   //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
-  /*Serial.print("UID tag : ");
+  //Serial.print("UID tag : ");
   String content = "";
   
   for(byte i = 0; i < mfrc522.uid.size; i++) {
@@ -75,16 +53,16 @@ void loop() {
   }
   Serial.println();
 
-  Serial.print("Message : ");
+  //Serial.print("Message : ");
   content.toUpperCase();
 
-  if(content.substring(1) == "") {
-    Serial.println("Authorized access");
+  if(content.substring(1) == "82 49 86 89") {
+    //Serial.println("Authorized access");
     Serial.println();
     delay(1000);
   }
   else {
-    Serial.println("Access denied");
+    Serial.println("error");
     delay(1000);
-  }*/
+  }
 }
